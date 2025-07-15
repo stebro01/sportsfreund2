4fa8f4fe2cbce77c1e91d0163a9d4739e9757d92
stebro <48401850+stebro01@users.noreply.github.com>
2025-07-15 13:47:45 +0200

    feat: add BaseDialog component

---
+++ src/components/BaseDialog.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-dialog v-model="modelValue">
+    <q-card class="my-popup-card text-center white-shadow">
+      <q-card-section v-if="title">{{ title }}</q-card-section>
+      <slot />
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  modelValue: Boolean,
+  title: String,
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const modelValue = computed({
+  get: () => props.modelValue,
+  set: (val) => emit("update:modelValue", val),
+});
+</script>
